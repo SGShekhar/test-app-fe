b5fa9c21468c9483474944906e242791bb3d857c
SGShekhar <shekhar@sggroup.sg>
2025-06-09 10:14:31 +0530

    testing test branch to qa branch cicd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Test New CICD and see your  QA  branch changes instantly.
+          new branch added test cicd
           </li>
         </ol>
 
